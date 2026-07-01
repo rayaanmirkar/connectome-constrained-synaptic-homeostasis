cb7ff7fe8e6640a080b09be8bea605192ccee310
iocle <ilmrexplorer@gmail.com>
2026-07-01 00:07:57 -0400

    commit

--- snn/main.py
+++ snn/main.py
@@ -5,25 +5,29 @@ prefs.codegen.target = "numpy"
 
 start_scope()
 
-tau_mem = 20*ms
-v_rest = 0
-v_max = 1
-v_reset = 0
-tau_atp = 2000*ms
-t_dep = 0.15p
-max_atp = 1.0
-I_sensory = 0.5 
-
-n_N = 10
+tau_mem = 20*ms # membrane-time constant
+v_rest = 0 # resting potential
+v_max =  # threshold
+v_reset = 0 # v after spike/reset
+tau_atp = 2000*ms #atp time constant; **how fast atp is replenished**
+t_dep = 0.15 # atp depression; **how much atp is lost when fireing.
+max_atp = 1.0 # max atp/how much is available
+I_sensory = 0.5 # constant sensory input
+n_N = 10 # no. neurons (n_N)
 
 eqs = '''
 dv/dt = (v_rest - v + I_sensory) / tau_mem : 1 (unless refractory)
 datp/dt = (max_atp - atp)/ tau_atp : 1
+tau: second
+tau_atp = second
+atp_rest
 '''
+
+
 #on_pre = 
 
-G = NeuronGroup(n_N, model = eqs, threshold='v> v_max', reset='v = 0*mV', method='euler')
-
+neurons = NeuronGroup(n_N, model = eqs, threshold='v> v_max', reset='v = 0*mV', method='euler')
+neurons.ATP = 1.0   
 
 
 
